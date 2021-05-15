5231f65abd2fad2074716e047f205f6d4ee7e990
Achraf Bilal <ach.20raf@outlook.fr>
2021-05-15 20:39:22 +0000

    Second commit
    Portfolio Page

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,187 +1,28 @@
 import React, { useState } from 'react'
 import "./experiences.scss"
-import Card from "../card/Card"
 export default function Experiences() {
-    const exp = [
-        {
-            id: 1,
-            title: "DEVELOPPEUR WEB/MOBILE",
-            company: "OPIA Solutions",
-            periode: {
-                from: new Date(2021, 2, 1),
-                to: new Date(2021, 4, 1)
-            },
-            projects: [
-                {
-                    title: "Application Mobile d'expertise automobile",
-                    technologies: [
-                        "Java ANDROID",
-                        "API PHP MySQL"
-                    ],
-                    assets: [
-                        {
-                            url: "/assets/android.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/java.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/php-mysql.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        }
-                    ],
-                },
-                {
-                    title: "Application Mobile d'expertise automobile",
-                    technologies: [
-                        "React Native",
-                        "API PHP MySQL"
-                    ],
-                    assets: [
-                        {
-                            url: "/assets/react-native.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/java.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/php-mysql.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "DEVELOPPEUR WEB/MOBILE",
-            company: "CDMA Solutions",
-            periode: {
-                from: new Date(2019, 9, 1),
-                to: new Date(2020, 3, 1)
-            },
-            projects: [
-                {
-                    title: "Application WEB d'expertise automobile",
-                    technologies: [
-                        "LARAVEL PHP",
-                        "VUE Js",
-                        "MySQL"
-                    ],
-                    assets: [
-                        {
-                            url: "/assets/laravel-vuejs.png",
-                            size: {
-                                width: 250, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/php-mysql.png",
-                            size: {
-                                width: 250, height: 150
-                            }
-                        }
-                    ]
-                },
-                {
-                    title: "Application Mobile d'expertise automobile",
-                    technologies: [
-                        "Java ANDROID",
-                        "API PHP MySQL "
-                    ],
-                    assets: [
-                        {
-                            url: "/assets/android.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/java.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/php-mysql.png",
-                            size: {
-                                width: 150, height: 150
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            id: 3,
-            title: "DEVELOPPEUR WEB",
-            company: "ISGI Casa",
-            periode: {
-                from: new Date(2020, 5, 1),
-                to: new Date(2020, 6, 1)
-            },
-            projects: [
-                {
-                    title: "Application WEB de gestion des inscriptions et rendez vous",
-                    technologies: [
-                        "LARAVEL PHP",
-                        "VUE Js",
-                        "MySQL"
-                    ]
-                    ,
-                    assets: [
-                        {
-                            url: "/assets/laravel-vuejs.png",
-                            size: {
-                                width: 250, height: 150
-                            }
-                        },
-                        {
-                            url: "/assets/php-mysql.png",
-                            size: {
-                                width: 250, height: 150
-                            }
-                        }
 
-                    ]
-                },
-
-            ]
-        }
-    ];
-    const [index, setIndex] = useState(0);
-
+    const list = ['featured', 'Web app', 'Mobile app', 'Design', 'Branding']
+    const [activeIndex, setActiveIndex] = useState(0);
     return (
         <div className="experiences" id="Experiences">
             <h1>Experiences</h1>
             <ul>
                 {
-                    exp.map((e, i) => <li onClick={() => setIndex(i)} className={e.id - 1 === index && 'active'}>{e.company}</li>)
+                    list.map
+                        (
+                            (e, i) =>
+                                <li key={i} onClick={() => setActiveIndex(i)} className={i === activeIndex && 'active'}>
+                                    {e}
+                                </li>
+                        )
                 }
-
             </ul>
             <div className="container">
-                {
-                    exp[index].projects.map((p, j) => (<Card content={p} key={j} />))
-                }
-
-
+                <div className="item">
+                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg " alt="" />
+                    <h3>Banking app</h3>
+                </div>
             </div>
         </div>
     )
